5927c02edf8ee4171e0fb233517e5b08114c39c8
Kai Kasurinen <kai.kasurinen@iki.fi>
2018-08-31 23:02:56 +0300

    ....

--- toukka/toukka/commands/__init__.py
+++ toukka/toukka/commands/__init__.py
@@ -4,10 +4,12 @@
 from . import toukka
 from . import fun
 from . import url
+from . import playing
 
 COMMANDS = [
     *toukka.COMMANDS,
     *url.COMMANDS,
+    *playing.COMMANDS,
     *fun.COMMANDS]
 
 NAMESPACE = 'toukka'
