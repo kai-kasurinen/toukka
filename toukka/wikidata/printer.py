46b8aa675357d5ad4c359b579d55e96bbd65db90
Kai Kasurinen <kai.kasurinen@iki.fi>
2018-12-02 18:23:08 +0200

    ...

--- toukka/wikidata/printer.py
+++ toukka/wikidata/printer.py
@@ -7,23 +7,33 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+def _iterlists(entity):
+    ''' same as entity.iterlists with exception handling '''
+    for prop in entity:
+        try:
+            v = entity.getlist(prop)
+        except wikidata.datavalue.DatavalueError as error:
+            logger.error(error)
+        yield prop, v
+
+
 def print_entity(entity):
     print()
+    _print_entity_oneline(entity)
+    _print_entity_all_props(entity)
+
+
+def _print_entity_oneline(entity):
     print('{}: {} ({})'.format(entity.id, entity.label, entity.description))
-    #print('label: {}'.format(entity.label))
-    #print('id: {}'.format(entity.id))
-    #print('description: {}'.format(entity.description))
 
-    key_value_lists = None
+def _print_entity_multiline(entity):
+    print('label: {}'.format(entity.label))
+    print('id: {}'.format(entity.id))
+    print('description: {}'.format(entity.description))
 
-    try:
-        key_value_lists = entity.lists()
-    # https://github.com/dahlia/wikidata/issues/6
-    except wikidata.datavalue.DatavalueError as error:
-        logger.error(error)
 
-    if key_value_lists is None:
-        return
+def _print_entity_all_props(entity):
+    key_value_lists = _iterlists(entity)
 
     for key, values in key_value_lists:
         values_to_print = list()
@@ -35,3 +45,4 @@ def print_entity(entity):
         if len(values_to_print) == 1:
            values_to_print  = values_to_print[0]
         print('\t{}: {}'.format(key.label, values_to_print))
+
