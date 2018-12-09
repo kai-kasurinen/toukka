1ba14c49848b811a672b93c24cc3112f41543101
Kai Kasurinen <kai.kasurinen@iki.fi>
2018-12-09 02:16:21 +0200

    ...

--- toukka/itunes/itunes.py
+++ toukka/itunes/itunes.py
@@ -5,10 +5,19 @@ from .api import iTunesAPI
 class iTunes:
     def __init__(self, session=None):
         self.api = iTunesAPI(session=session)
-        self.country = 'FI'
+        self._country = 'FI'
+        self._lang = 'en_us'
 
-    def search(self, **kwargs):
-        return self.api.search(**kwargs)
+    def search(self, country=None, lang=None, **kwargs):
+        if country is None:
+            country = self._country
+        if lang is None:
+            lang = self._lang
+        return self.api.search(country=country, lang=lang, **kwargs)
 
-    def lookup(self, **kwargs):
-        return self.api.lookup(**kwargs)
+    def lookup(self, country=None, lang=None, **kwargs):
+        if country is None:
+            country = self._country
+        if lang is None:
+            lang = self._lang
+        return self.api.lookup(country=country, lang=lang, **kwargs)
