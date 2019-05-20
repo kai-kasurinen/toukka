4ae1a47d825fbfa007dd921df1a483fc56f57c7a
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-05-20 03:32:21 +0300

    ...

--- toukka/experimental/commands/__init__.py
+++ toukka/experimental/commands/__init__.py
@@ -4,16 +4,17 @@
 #from . import toukka
 #from . import fun
 #from . import url
-from . import spotify_playing
-from . import spotify_playing_watcher
-from . import spotify_playlist_manager
-from . import spotify_artist
+#from . import spotify_playing
+#from . import spotify_playing_watcher
+#from . import spotify_playlist_manager
+#from . import spotify_artist
 
 COMMANDS = [
-    *spotify_playing.COMMANDS,
-    *spotify_playing_watcher.COMMANDS,
-    *spotify_playlist_manager.COMMANDS,
-    *spotify_artist.COMMANDS]
+#    *spotify_playing.COMMANDS,
+#    *spotify_playing_watcher.COMMANDS,
+#    *spotify_playlist_manager.COMMANDS,
+#    *spotify_artist.COMMANDS
+]
 
 NAMESPACE = 'experimental'
 
