9596ddc6acdc2937177360a21dfa80f2bd499f9d
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-09-30 08:21:23 +0300

    ...

--- sopiva/spotify/client_cached.py
+++ sopiva/spotify/client_cached.py
@@ -7,7 +7,7 @@ import sopiva.cache.dogpile
 class CachedSpotify(spotipy.Spotify):
 
     @sopiva.cache.dogpile.region.cache_on_arguments()
-    def track(self, track_id, market):
+    def track(self, track_id, market=None):
         return super().track(track_id, market=market)
 
     @sopiva.cache.dogpile.region.cache_on_arguments()
@@ -15,7 +15,7 @@ class CachedSpotify(spotipy.Spotify):
         return super().artist(artist_id)
 
     @sopiva.cache.dogpile.region.cache_on_arguments()
-    def album(self, album_id, market):
+    def album(self, album_id, market=None):
         return super().album(album_id, market=market)
 
 
