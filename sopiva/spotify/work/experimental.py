ac0a70abd76b521c8f84c38dc507c2aa3ce35f94
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-10-03 18:24:45 +0300

    ...

--- sopiva/spotify/work/experimental.py
+++ sopiva/spotify/work/experimental.py
@@ -49,18 +49,15 @@ _RANGES_DESCRIPTION = {
 def current_user_top_artists(time_range: 'short, medium or long'):
     spotify = get_spotify()
 
-    if time_range not in ('short', 'medium', 'long'):
-        raise Exception()
-
-    time_range = '{}_term'.format(time_range)
-
-    print('{}: {}'.format(time_range, _RANGES_DESCRIPTION.get(time_range)))
-    print()
+    time_range = f'{time_range}_term'
 
     results = spotify.current_user_top_artists(time_range=time_range, limit=50)
 
     def unpack(s):
         return ', '.join(map(str, s))
+
+    print(f'{time_range}: {_RANGES_DESCRIPTION.get(time_range)}')
+    print()
 
     for i, item in enumerate(results.items, start=1):
         print(f'{i:2} {item.name:50} {unpack(item.genres)}')
