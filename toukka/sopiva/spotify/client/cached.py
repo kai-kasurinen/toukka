3275a98070efad70f072d9793d26c946b5cb28ea
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-10-17 10:50:09 +0300

    ...

--- toukka/sopiva/spotify/client/cached.py
+++ toukka/sopiva/spotify/client/cached.py
@@ -2,31 +2,36 @@
 
 from typing import Union
 
-import toukka.cache.dogpile
+from spotipy.model import FullTrack, FullArtist, FullAlbum, AudioFeatures
 
+import toukka.cache.dogpile
 from .extented import SpotifyExtended
+
 
 class SpotifyCached(SpotifyExtended):
 
     @toukka.cache.dogpile.region.cache_on_arguments()
     def track(self,
               track_id: str,
-              market: Union[str, None] = 'from_token'):
+              market: Union[str, None] = 'from_token'
+              ) -> FullTrack:
         return super().track(track_id, market=market)
 
     @toukka.cache.dogpile.region.cache_on_arguments()
     def artist(self,
-               artist_id: str):
+               artist_id: str
+               ) -> FullArtist:
         return super().artist(artist_id)
 
     @toukka.cache.dogpile.region.cache_on_arguments()
     def album(self,
               album_id: str,
-              market: Union[str, None] = 'from_token'):
+              market: Union[str, None] = 'from_token'
+              ) -> FullAlbum:
         return super().album(album_id, market=market)
 
     @toukka.cache.dogpile.region.cache_on_arguments()
-    def track_audio_features(self, track_id: str):
+    def track_audio_features(self, track_id: str) -> AudioFeatures:
         return super().track_audio_features(track_id)
 
 # END
--- toukka/sopiva/spotify/client/extented.py
+++ toukka/sopiva/spotify/client/extented.py
@@ -1,14 +1,12 @@
 #
 
+from typing import Generator
 import logging
 
 import deprecated
-
 import spotipy
-import spotipy.convert
-import spotipy.model
-import spotipy.model.paging
-
+from spotipy.model.paging import Paging
+from spotipy.model.base import Item
 
 logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
@@ -16,33 +14,25 @@ logger = logging.getLogger(__name__)
 
 class SpotifyExtended(spotipy.Spotify):
 
-    def iterate_pages_from_paging(self,
-                                  paging: spotipy.model.paging.OffsetPaging):
+    def pages_from_paging(self,
+                          paging: Paging
+                          ) -> Generator[Paging, None, None]:
         '''pages from Paging generator'''
         yield paging
         while paging.next:
             paging = self.next(paging)
             yield paging
 
-    def iterate_items_from_paging(self,
-                                  paging: spotipy.model.paging.OffsetPaging):
+    def items_from_paging(self,
+                          paging: Paging
+                          ) -> Generator[Item, None, None]:
         '''all items from Paging generator'''
         for page in self.iterate_pages_from_paging(paging):
             yield from page.items
 
     # FIXME: better?
-    pages_from_paging = iterate_pages_from_paging
-    items_from_paging = iterate_items_from_paging
-
-    @deprecated.deprecated
-    def pager(self,
-              paging: spotipy.model.paging.OffsetPaging):
-        return self.iterate_pages_from_paging(paging)
-
-    @deprecated.deprecated
-    def all_items_from_paging(self,
-                              paging: spotipy.model.paging.OffsetPaging):
-        return self.iterate_items_from_paging(paging)
+    iterate_pages_from_paging = pages_from_paging
+    iterate_items_from_paging = items_from_paging
 
 
 # END
