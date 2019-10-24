e54456c57c89738b33f8ff310932db6530ec3cae
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-10-24 23:13:29 +0300

    ...

--- toukka/sopiva/spotify/client/extented.py
+++ toukka/sopiva/spotify/client/extented.py
@@ -10,25 +10,26 @@ from spotipy.model.base import Item
 
 class SpotifyExtended(spotipy.client.Spotify):
 
-    def pages_from_paging(self,
-                          paging: Paging
-                          ) -> Generator[Paging, None, None]:
+    def all_pages_from_paging(self,
+                              paging: Paging
+                              ) -> Generator[Paging, None, None]:
         '''all pages from Paging generator'''
         yield paging
         while paging.next:
             paging = self.next(paging)
             yield paging
 
-    def items_from_paging(self,
-                          paging: Paging
-                          ) -> Generator[Item, None, None]:
+    def all_items_from_paging(self,
+                              paging: Paging
+                              ) -> Generator[Item, None, None]:
         '''all items from Paging generator'''
-        for page in self.pages_from_paging(paging):
+        for page in self.all_pages_from_paging(paging):
             yield from page.items
 
     # FIXME: remove
     iterate_pages_from_paging = pages_from_paging
     iterate_items_from_paging = items_from_paging
-    all_items_from_paging = items_from_paging
+    items_from_paging = all_items_from_paging
+    pages_from_paging = all_pages_from_paging
 
 # END
