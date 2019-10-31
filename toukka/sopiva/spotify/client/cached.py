1477b241ba5df89429e4c28bc5252ee8c82336b6
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-10-31 05:03:41 +0200

    ...

--- toukka/sopiva/spotify/client/cached.py
+++ toukka/sopiva/spotify/client/cached.py
@@ -1,11 +1,14 @@
 #
 
 from typing import Union
-
+import logging
 import spotipy.client
 from spotipy.model import FullTrack, FullArtist, FullAlbum, AudioFeatures
 
 import toukka.cache.dogpile
+
+
+logger = logging.getLogger(__name__)
 
 
 # TODO: rename cached methods
@@ -21,6 +24,8 @@ class SpotifyCached(spotipy.client.Spotify):
               track_id: str,
               market: Union[str, None] = 'from_token'
               ) -> FullTrack:
+        if market == 'from_token':
+            logger.warning('market from_token and caching')
         return super().track(track_id, market=market)
 
     @toukka.cache.dogpile.redis.cache_on_arguments()
@@ -34,6 +39,8 @@ class SpotifyCached(spotipy.client.Spotify):
               album_id: str,
               market: Union[str, None] = 'from_token'
               ) -> FullAlbum:
+        if market == 'from_token':
+            logger.warning('market from_token and caching')
         return super().album(album_id, market=market)
 
     # NOTE: not cached by cachecontrol at all
@@ -41,6 +48,8 @@ class SpotifyCached(spotipy.client.Spotify):
     def track_audio_features(self, track_id: str) -> AudioFeatures:
         return super().track_audio_features(track_id)
 
-
+    @toukka.cache.dogpile.redis.cache_on_arguments()
+    def track_audio_analysis(self, track_id: str) -> AudioAnalysis:
+        return super().track_audio_analysis(track_id)
 
 # END
