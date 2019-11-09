4554fb6e6b1f5fc785b3dda803296a86e8879c6c
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-11-09 09:41:02 +0200

    ...

--- toukka/sopiva/spotify/client/cached.py
+++ toukka/sopiva/spotify/client/cached.py
@@ -8,6 +8,8 @@ from toukka.cache.dogpile import redis
 # TODO: handle from_token better
 # TODO: do something when pickling fails?
 
+region = redis
+
 
 def check_from_token(f):
     @functools.wraps(f)
@@ -29,25 +31,25 @@ class SpotifyCached(Spotify):
 
     # cache-control: public, max-age>0
     track = check_from_token(
-        redis.cache_on_arguments()(Spotify.track))
+        region.cache_on_arguments()(Spotify.track))
     tracks = check_from_token(
-        redis.cache_on_arguments()(Spotify.tracks))
-    artist = redis.cache_on_arguments()(Spotify.artist)
+        region.cache_on_arguments()(Spotify.tracks))
+    artist = region.cache_on_arguments()(Spotify.artist)
     artist_albums = check_from_token(
-        redis.cache_on_arguments()(Spotify.artist_albums))
-    artist_related_artists = redis.cache_on_arguments()(Spotify.artist_related_artists)
+        region.cache_on_arguments()(Spotify.artist_albums))
+    artist_related_artists = region.cache_on_arguments()(Spotify.artist_related_artists)
     artist_top_tracks = check_from_token(
-        redis.cache_on_arguments()(Spotify.artist_top_tracks))
+        region.cache_on_arguments()(Spotify.artist_top_tracks))
     album = check_from_token(
-        redis.cache_on_arguments()(Spotify.album))
+        region.cache_on_arguments()(Spotify.album))
     albums = check_from_token(
-        redis.cache_on_arguments()(Spotify.albums))
+        region.cache_on_arguments()(Spotify.albums))
     album_tracks = check_from_token(
-        redis.cache_on_arguments()(Spotify.album_tracks))
+        region.cache_on_arguments()(Spotify.album_tracks))
 
     # cache-control: private, max-age=0
-    track_audio_features = redis.cache_on_arguments()(Spotify.track_audio_features)
-    track_audio_analysis = redis.cache_on_arguments()(Spotify.track_audio_analysis)
+    track_audio_features = region.cache_on_arguments()(Spotify.track_audio_features)
+    track_audio_analysis = region.cache_on_arguments()(Spotify.track_audio_analysis)
 
 
 # END
