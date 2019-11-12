812dfaa682cb4d4e91385b384f55de9728cd97ef
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-11-12 04:43:42 +0200

    ...

--- toukka/sopiva/spotify/commands/player.py
+++ toukka/sopiva/spotify/commands/player.py
@@ -11,7 +11,7 @@ from toukka.sopiva.spotify.printer.first import printer
 from toukka.sopiva.spotify.cli import cli_root
 
 # NOTE: most of current player commands needs premium account
-# FIXME: update all commands use new spotipy
+# NOTE: .. so not tested
 
 
 @cli_root.group()
@@ -20,46 +20,62 @@ def playback():
 
 
 @playback.command()
+@click.option('--context-uri')
+@click.option('--track-ids')
+@click.option('--offset')
+@click.option('--position-ms')
+@click.option('--device-id')
 def start(context_uri: str = None,
           track_ids: list = None,
           offset: Union[int, str] = None,
           position_ms: int = None,
           device_id: str = None
           ):
-    return get_spotify().playback_pause(device_id=device_id)
+    return get_spotify().playback_start(**locals())
 
 
 @playback.command()
+@click.option('--device-id')
 def pause(device_id=None):
-    return get_spotify().playback_pause(device_id=device_id)
+    return get_spotify().playback_pause(**locals())
 
 
 @playback.command()
+@click.option('--device-id')
 def next(device_id=None):
-    return get_spotify().playback_next(device_id=device_id)
+    return get_spotify().playback_next(**locals())
 
 
 @playback.command()
+@click.option('--device-id')
 def previous(device_id=None):
-    return get_spotify().playback_next(device_id=device_id)
+    return get_spotify().playback_next(**locals())
 
 
 @playback.command()
+@click.argument('position_ms')
+@click.option('--device-id')
 def seek(position_ms: int, device_id=None):
-    return get_spotify().playback_seek(position_ms, device_id=device_id)
+    return get_spotify().playback_seek(locals())
 
 
 @playback.command()
+@click.argument('state')
+@click.option('--device-id')
 def repeat(state: str, device_id=None):
-    return get_spotify().playback_repeat(state, device_id=device_id)
+    return get_spotify().playback_repeat(**locals())
 
 
 @playback.command()
+@click.argument('percent')
+@click.option('--device-id')
 def volume(percent: int, device_id=None):
-    return get_spotify().playback_volume(percent, device_id=device_id)
+    return get_spotify().playback_volume(locals())
 
 
 @playback.command()
+@click.argument('state')
+@click.option('--device-id')
 def shuffle(state: bool, device_id=None):
     return get_spotify().playback_shuffle(state, device_id=device_id)
 
@@ -83,6 +99,7 @@ def currently_playing(market=None):
 
 
 @playback.command()
+@click.option('--device-id')
 def transfer_playback(device_id, force_play: bool = False):
     return get_spotify().playback_transfer(device_id, force_play=force_play)
 
