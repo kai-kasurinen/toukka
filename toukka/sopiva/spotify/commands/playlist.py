897219af6d1ff2d69fb0086180a2a24e25e8148d
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-11-13 23:53:04 +0200

    ...

--- toukka/sopiva/spotify/commands/playlist.py
+++ toukka/sopiva/spotify/commands/playlist.py
@@ -30,24 +30,21 @@ def current_user_playlists():
     spotify = get_spotify()
     paging = spotify.current_user_playlists()
     print(f'user has {paging.total} playlists')
-    playlists = spotify.all_items_from_paging(paging)
 
-    for p in playlists:
-        # FIXME: better printing
-        print(f'name: {p.name}',
-              f', number of songs: {p.tracks.total}',
-              f', id: {p.id}')
+    for playlist in spotify.all_items_from_paging(paging):
+        printer(playlist)
 
 
 # FIXME: move
 @playlist.command()
+@click.argument('user')
 def user_playlists_info(user):
     '''gets playlists of a user'''
 
     spotify = get_spotify()
     paging = spotify.playlists(user)
     print(f'user has {paging.total} playlists')
-    playlists = spotify.items_from_paging(paging)
+    playlists = spotify.all_items_from_paging(paging)
 
     own = [p for p in playlists if p.owner.id == user]
     public = [p for p in playlists if p.public is True]
@@ -58,48 +55,14 @@ def user_playlists_info(user):
     print(f'total collaborative playlists: {len(collaborative)}')
 
 
-# FIXME: fix and move
 @playlist.command()
-def user_playlists(user,
-                   filter_own=False,
-                   filter_public=False,
-                   filter_collaborative=False,
-                   filter_by_userid=None):
-    '''gets playlists of a user'''
-    toukka = Toukka()
-
-    paging = toukka.sp.user_playlists(user)
-    playlists = toukka.sp.aggregate_paging_results(paging)
-    #playlists = toukka.sp.user_playlists_all(user)
-
-
-    if filter_own:
-        playlists = [p for p in playlists if p.get('owner').get('id') == user]
-
-    if filter_public:
-        playlists = [p for p in playlists if p.get('public') is True]
-
-    if filter_collaborative:
-        playlists = [p for p in playlists if p.get('collaborative') is True]
-
-    if filter_by_userid:
-        playlists = [p for p in playlists if p.get('owner').get('id') == filter_by_userid]
-
-    _print_playlists(playlists)
-    print('\nfiltered to {} of total {}'.format(len(playlists), paging.get('total')))
-
-
-def _print_playlists(playlists, one_line=True):
-
-    if one_line:
-        line = "name: {name:40} id: {id:30} owner: {owner[id]:30} tracks: {tracks[total]:5} flags: {flags}"
-    else:
-        line = "name: {name}\nid: {id}\nowner: {owner[id]}\nflags: {flags}\ntracks: {tracks[total]}\n"
-
-    for playlist in playlists:
-        print(line.format(
-            **playlist,
-            flags=_list_to_string(_get_flags(playlist, ['public', 'collaborative']))))
+@click.argument('user')
+def user_playlists(user):
+    spotify = get_spotify()
+    paging = spotify.playlists(user)
+    print(f'user has {paging.total} playlists')
+    for playlist in spotify.all_items_from_paging(paging):
+        printer(playlist)
 
 
 @playlist.command(name='info')
