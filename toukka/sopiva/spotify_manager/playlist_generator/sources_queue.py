e1a5628b9a9839c6af75baccfc66f2193b981b2a
Kai Kasurinen <kai.kasurinen@iki.fi>
2019-11-19 14:41:17 +0200

    ...

--- toukka/sopiva/spotify_manager/playlist_generator/sources_queue.py
+++ toukka/sopiva/spotify_manager/playlist_generator/sources_queue.py
@@ -1,16 +1,18 @@
 #
 
+from typing import Generator, Any, Deque
+
 import collections
 
 
 class SourcesQueue:
-    def __init__(self):
-        self.sources_queue = collections.deque()
+    def __init__(self) -> None:
+        self.sources_queue: Deque = collections.deque()
 
-    def add(self, source):
+    def add(self, source) -> None:
         self.sources_queue.append(source)
 
-    def generator(self):
+    def generator(self) -> Generator[Any, None, None]:
         while True:
             try:
                 yield from self.sources_queue.popleft()
