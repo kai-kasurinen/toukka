282f7edc0c8adadb1c4c385d69998563f565798f
Kai Kasurinen <kai.kasurinen@iki.fi>
2020-01-18 03:56:34 +0200

    ...

--- toukka/sopiva/spotify_manager/playlist_generator/util.py
+++ toukka/sopiva/spotify_manager/playlist_generator/util.py
@@ -38,6 +38,7 @@ def scramble_generator(generator: Generator, buffer_size: int) -> Generator[Any,
             return
 
 
+# TODO: convert iterable to list (so all items is used) and randomize list
 def take_random_items_generator(iterable, count=1):
     yield from more_itertools.take(count, scramble_generator(iterable, 100))
 
