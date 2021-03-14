4f9bf22535e11506348dedc34d16243a76996e32
Kai Kasurinen <kai.kasurinen@iki.fi>
2021-03-14 16:30:38 +0200

    ...

--- toukka/sopiva/spotify_manager/analyze/artist_related_artists.py
+++ toukka/sopiva/spotify_manager/analyze/artist_related_artists.py
@@ -16,42 +16,6 @@ def artist_related_artists_test(artist_uri):
         artist_type, artist_id = spotify.convert.from_uri(artist_uri)
         return artist_id
 
-    def get_related_artists(artist_id):
-        return [artist.id for artist in spotify.artist_related_artists(artist_id)]
-
-    def build_related_artists_graph(root_id, max_depth=10):
-
-        # init
-        seen = set()
-        graph = networkx.MultiDiGraph()
-
-        # recursive loop function
-        def do_recurse(parent_id, current_id, depth):
-
-            if depth > max_depth:
-                return
-
-            if current_id in seen:
-                return
-            else:
-                seen.add(current_id)
-
-            print(f'parent: {parent_id}, current: {current_id} depth: {depth}')
-
-            childs = get_related_artists(current_id)
-
-            graph.add_node(current_id)
-
-            for child_id in childs:
-                graph.add_edge(current_id, child_id)
-
-            for child_id in childs:
-                do_recurse(current_id, child_id, depth+1)
-
-        do_recurse(None, root_id, 0)
-        print(f'seen: {len(seen)}')
-        return graph
-
     def analyze_graph(graph):
         print(f'graph has {len(graph.nodes)} nodes and {len(graph.edges)} edges')
         print(f'successors: {list(graph.successors(root_artist_id))}')
@@ -62,5 +26,42 @@ def artist_related_artists_test(artist_uri):
     related_artists_graph = build_related_artists_graph(root_artist_id)
     analyze_graph(related_artists_graph)
 
+
+def build_related_artists_graph(root_id, max_depth=10):
+
+    # init
+    spotify = get_spotify()
+    seen = set()
+    graph = networkx.MultiDiGraph()
+
+    def get_related_artists(artist_id):
+        return [artist.id for artist in spotify.artist_related_artists(artist_id)]
+
+    # recursive loop function
+    def do_recurse(parent_id, current_id, depth):
+
+        if depth > max_depth:
+            return
+
+        if current_id in seen:
+            return
+        else:
+            seen.add(current_id)
+
+        print(f'parent: {parent_id}, current: {current_id} depth: {depth}')
+
+        childs = get_related_artists(current_id)
+
+        graph.add_node(current_id)
+
+        for child_id in childs:
+            graph.add_edge(current_id, child_id)
+
+        for child_id in childs:
+            do_recurse(current_id, child_id, depth+1)
+
+    do_recurse(None, root_id, 0)
+    print(f'seen: {len(seen)}')
+    return graph
 
 # END
