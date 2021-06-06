18d1fad2496cdd0e20ee6a25aeafc9f6f20f1168
Kai Kasurinen <kai.kasurinen@iki.fi>
2021-06-06 11:08:55 +0300

    ...

--- toukka/logger/simple.py
+++ toukka/logger/simple.py
@@ -1,18 +1,16 @@
 #
 
 import logging
-# import autologging
-
-# TODO:  https://pythonhosted.org/Autologging/examples-separate.html
 
 
 def init_logging():
-    # FIXME: fix format
+
     logging.basicConfig(
         level=logging.INFO,
-        #format='%(asctime)s %(name)s %(levelname)s %(message)s'
-        format='%(asctime)s %(name)s.%(funcName)s %(levelname)s %(message)s'
+        # format='%(asctime)s %(name)s %(levelname)s %(message)s'
+        # format='%(asctime)s %(name)s.%(funcName)s %(levelname)s %(message)s'
         # format='%(levelname)s:%(filename)s,%(lineno)d:%(name)s.%(funcName)s:%(message)s'
+        format='%(asctime)s %(levelname)-8s %(name)s.%(funcName)s: %(message)s'
         )
     logging.captureWarnings(True)
 
@@ -20,8 +18,5 @@ def init_logging():
 def set_logging_level(level):
     logging.getLogger().setLevel(level)
 
-
-# def set_logging_level_to_trace():
-#     set_logging_level(autologging.TRACE)
 
 # END
--- toukka/logger/to_file.py
+++ toukka/logger/to_file.py
@@ -5,11 +5,13 @@ import logging
 
 from xdg.BaseDirectory import save_cache_path
 
+
 def set_logging_file():
     file = os.path.join(save_cache_path('toukka'), 'toukka.log')
-    fh = logging.FileHandler(file)
+    file_handler = logging.FileHandler(file)
+    # FIXME: modify
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    fh.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
     # FIXME: Not working?
-    fh.setLevel(logging.DEBUG)
-    logging.getLogger('').addHandler(fh)
+    file_handler.setLevel(logging.DEBUG)
+    logging.getLogger('').addHandler(file_handler)
