8618a79660816b199c88c24afe4744be04fb8954
Kai Kasurinen <kai.kasurinen@iki.fi>
2021-07-27 15:42:20 +0300

    ...

--- toukka/sopiva/spotify/commands/user.py
+++ toukka/sopiva/spotify/commands/user.py
@@ -6,19 +6,19 @@ import click
 
 from toukka.sopiva.spotify.util import get_spotify
 from toukka.sopiva.spotify.cli import cli_root
-
+from toukka.printer import printer
 
 @cli_root.command()
 @click.argument('user_id')
 def user(user_id: str):
     ''' get public profile information about a Spotify user.'''
-    return get_spotify().user(user_id).pprint()
+    get_spotify().user(user_id).pprint()
 
 
 @cli_root.command()
 def me():
     '''get current user information'''
-    return get_spotify().current_user().pprint()
+    get_spotify().current_user().pprint()
 
 
 # END
