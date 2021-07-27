83fdb47f27b7068b733cb8d24449f4ff008ef28a
Kai Kasurinen <kai.kasurinen@iki.fi>
2021-07-27 12:03:20 +0300

    ...

--- toukka/sopiva/spotify/commands/personalization.py
+++ toukka/sopiva/spotify/commands/personalization.py
@@ -3,6 +3,9 @@
 import click
 
 from toukka.sopiva.spotify.util import get_spotify
+from toukka.printer import printer
+
+
 from ..cli import cli_root
 
 
@@ -19,34 +22,64 @@ _RANGES_DESCRIPTION = {
 
 
 @personalization.command('top-tracks')
-@click.argument('time-range', type=click.Choice(['short', 'medium', 'long']))
-def current_user_top_tracks(time_range):
+@click.option('--time-range', type=click.Choice(['short', 'medium', 'long']), default='long')
+@click.option('--output-format', type=click.Choice(['default', 'long']), default='default')
+def current_user_top_tracks(
+        time_range,
+        output_format
+        ):
+
     spotify = get_spotify()
     time_range = f'{time_range}_term'
-    print(f'{time_range}: {_RANGES_DESCRIPTION.get(time_range)}')
-    print()
     paging = spotify.current_user_top_tracks(time_range=time_range, limit=50)
-    for i, item in enumerate(spotify.all_items(paging), start=1):
-        artists = ', '.join(artist.name for artist in item.artists)
-        print(f'{i:2} {item.name:50} {artists}')
 
-
-@personalization.command('top-artists')
-@click.argument('time-range', type=click.Choice(['short', 'medium', 'long']))
-def current_user_top_artists(time_range):
-    spotify = get_spotify()
-    time_range = f'{time_range}_term'
-    print(f'{time_range}: {_RANGES_DESCRIPTION.get(time_range)}')
-    print()
-    paging = spotify.current_user_top_artists(time_range=time_range, limit=50)
-    artists = spotify.all_items(paging)
-
+    # NOTE: sometimes spotify returns 0 items
     if paging.total < 1:
         paging.pprint()
 
+    print(f'{time_range}: {_RANGES_DESCRIPTION.get(time_range)}')
+    print()
+
+    for i, item in enumerate(spotify.all_items(paging), start=1):
+
+        if output_format == 'long':
+            printer(item)
+        elif output_format == 'default':
+            artists = ', '.join(artist.name for artist in item.artists)
+            print(f'{i:2} {item.name:50} {artists}')
+        else:
+            raise Exception()
+
+
+@personalization.command('top-artists')
+@click.option('--time-range', type=click.Choice(['short', 'medium', 'long']), default='long')
+@click.option('--output-format', type=click.Choice(['default', 'long']), default='default')
+def current_user_top_artists(
+        time_range,
+        output_format
+        ):
+
+    spotify = get_spotify()
+    time_range = f'{time_range}_term'
+
+    paging = spotify.current_user_top_artists(time_range=time_range, limit=50)
+    artists = spotify.all_items(paging)
+
+    # NOTE: sometimes spotify returns 0 items
+    if paging.total < 1:
+        paging.pprint()
+
+    print(f'{time_range}: {_RANGES_DESCRIPTION.get(time_range)}')
+    print()
+
     for i, item in enumerate(artists, start=1):
-        genres = ', '.join(item.genres)
-        print(f'{i:2} {item.name:50} {genres}')
+        if output_format == 'long':
+            printer(item)
+        elif output_format == 'default':
+            genres = ', '.join(item.genres)
+            print(f'{i:2} {item.name:50} {genres}')
+        else:
+            raise Exception()
 
 
 # END
