ecceb2bbf390974dcc3b262f567294ee6e04c456
Kai Kasurinen <kai.kasurinen@iki.fi>
2021-09-16 15:13:09 +0300

    ...

--- toukka/sopiva/spotify/commands/album.py
+++ toukka/sopiva/spotify/commands/album.py
@@ -24,3 +24,27 @@ def album(uri,
             printer(track)
 
 # END
+
+
+@cli_root.command()
+@click.argument('uri')
+@click.option('--market')
+@click.option('--full', is_flag=True)
+def album_tracks(
+        uri,
+        market: str = None,
+        full: bool = False
+        ):
+
+    spotify = get_spotify()
+    uri_type, uri_id = spotify.convert.from_uri(uri)
+    album_tracks_pager = spotify.album_tracks(uri_id, market=market)
+    album_tracks = spotify.all_items(album_tracks_pager)
+
+    for album_track in album_tracks:
+        if full:
+            printer(spotify.track(album_track.id))
+        else: 
+            printer(album_track)
+
+# END
