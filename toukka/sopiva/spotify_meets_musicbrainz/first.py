8563ccc92b8b9a4dda2ac8e4fb79167f201122cf
Kai Kasurinen <kai.kasurinen@iki.fi>
2021-11-17 18:59:08 +0200

    ...

--- toukka/sopiva/spotify_meets_musicbrainz/first.py
+++ toukka/sopiva/spotify_meets_musicbrainz/first.py
@@ -1,10 +1,15 @@
 #
 
+import logging
+
 import toukka.sopiva.spotify.printer.first
 
 from toukka.sopiva.spotify.util import get_spotify
 from toukka.sopiva.metabrainz import musicbrainzngs
 from toukka.printer.first import printer
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 def print_currently_listening():
@@ -14,8 +19,57 @@ def print_currently_listening():
 
     if currently_playing.currently_playing_type.name == 'track':
         track = currently_playing.item
+    else:
+        print('Not playing...')
+        return
 
-    printer(track)
-    printer(track.album)
+    # printer(track)
+    # printer(track.album)
+    # for artist in track.artists:
+    #     printer(artist)
+
+    album_url = spotify.convert.to_url(track.album.type, track.album.id)
+    artist_urls = list()
+
     for artist in track.artists:
-        printer(artist)
+        artist_url = spotify.convert.to_url(artist.type, artist.id)
+        artist_urls.append(artist_url)
+
+    print(album_url)
+    print(artist_urls)
+
+    get_entity_mbids_by_url('release', album_url)
+
+    for artist_url in artist_urls:
+        get_entity_mbids_by_url('artist', artist_url)
+
+
+def get_entity_mbids_by_url(entity_type, url):
+    logger.debug('get entity (%s) mbids by url %s', entity_type, url)
+
+    try:
+        result = musicbrainzngs.browse_urls(
+            resource=url,
+            includes=musicbrainzngs.VALID_BROWSE_INCLUDES.get('url'))
+    except musicbrainzngs.musicbrainz.ResponseError as error:
+        if error.cause.code == 404:
+            logger.debug('not found')
+            return []
+        else:
+            raise
+
+    mbids = list()
+
+    if result:
+        logger.debug('found %s relations from musicbrainz', len(result.get('relations')))
+        for relation in result.get('relations'):
+            target_type = relation.get('target-type')
+            if target_type == entity_type:
+                mbids.append(relation.get(entity_type).get('id'))
+            else:
+                logger.debug('warn: wrong target_type (%s != %s) on relation', target_type, entity_type)
+    else:
+        logger.debug('failed, no result')
+    return mbids
+
+# END
