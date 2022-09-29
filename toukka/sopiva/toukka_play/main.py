9a9fabead91879a142520b65a4707211fe60af1f
Kai Kasurinen <kai.kasurinen@iki.fi>
2022-09-29 09:56:54 +0300

    ...

--- toukka/sopiva/toukka_play/main.py
+++ toukka/sopiva/toukka_play/main.py
@@ -71,7 +71,7 @@ kwargs_for_genre_artists_re = {
     'expand_artist_to_related_artists': True,
     'expand_album_to_tracks': True,
     'randomize_artists': True,
-    'randomize_sources': True
+    'randomize_genres': True
 }
 
 kwargs_for_uri = {
