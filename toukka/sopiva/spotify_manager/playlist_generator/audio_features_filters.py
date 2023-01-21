c176c6832116c19ae1ce5e64a2e75c6c4cbcf881
Kai Kasurinen <kai.kasurinen@iki.fi>
2023-01-21 16:07:28 +0200

    ...

--- toukka/sopiva/spotify_manager/playlist_generator/audio_features_filters.py
+++ toukka/sopiva/spotify_manager/playlist_generator/audio_features_filters.py
@@ -9,10 +9,9 @@ logger.setLevel(logging.DEBUG)
 
 
 def is_album_instrumental(spotify, album_id):
-    
     album = spotify.album(album_id)
     track_ids = [track.id for track in album.tracks.items]
-    
+
     tracks_audio_features = spotify.tracks_audio_features(track_ids)
     # drop Nones
     tracks_audio_features = [item for item in tracks_audio_features if item is not None]
@@ -26,11 +25,10 @@ def is_album_instrumental(spotify, album_id):
     instrumentalness_mean = tracks_audio_features_df['instrumentalness'].mean()
 
     logger.debug(instrumentalness_mean)
-    
+
     if instrumentalness_mean > 0.5:
         return True
     else:
         return False
-    
 
-    
+# END
