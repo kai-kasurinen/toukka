7a2de4058be6104b0bd390590ff44e48362fa141
Kai Kasurinen <kai.kasurinen@iki.fi>
2023-01-24 10:55:12 +0200

    ...

--- toukka/sopiva/spotify_manager/analyze/album.py
+++ toukka/sopiva/spotify_manager/analyze/album.py
@@ -14,12 +14,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-# TODO: first test and then split
-# TODO: use pandas (dataclasses support needs 1.1)
-
-
-# FIXME: WRITE!
-
 def analyze_album(album_uri):
     spotify = get_spotify()
 
@@ -30,16 +24,17 @@ def analyze_album(album_uri):
     print()
 
     album = spotify.album(album_id)
-    album_tracks = spotify.all_items(album.tracks)
+    album_tracks = list(spotify.all_items(album.tracks))
     track_ids = [track.id for track in album_tracks]
+    tracks = list(spotify.tracks(track_ids))
     album_audio_features = spotify.tracks_audio_features(track_ids)
     album_audio_features = [item for item in album_audio_features if item is not None]
     album_audio_features_df = pandas.DataFrame(album_audio_features)
-    album_tracks_df = pandas.DataFrame(album_tracks)
+    album_tracks_df = pandas.DataFrame(tracks)
 
     # TODO: simpletrack does not include popularity
-    # print(album_tracks_df['popularity'].describe())
-    # print()
+    print(album_tracks_df['popularity'].describe())
+    print()
     print(album_audio_features_df.describe())
 
 # END
