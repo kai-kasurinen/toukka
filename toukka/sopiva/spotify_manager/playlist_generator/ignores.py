b5872f9b620cb916f796ab6aa4e49aceda0158a0
Kai Kasurinen <kai.kasurinen@iki.fi>
2023-07-08 14:24:44 +0300

    ...

--- toukka/sopiva/spotify_manager/playlist_generator/ignores.py
+++ toukka/sopiva/spotify_manager/playlist_generator/ignores.py
@@ -6,14 +6,135 @@ VARIOUS_ARTISTS = [
 ]
 
 CLASSICAL_ARTISTS = [
-    'spotify:artist:7y97mc3bZRFXzT2szRM4L4',    # Frédéric Chopin
-    'spotify:artist:2UqjDAXnDxejEyE0CzfUrZ',    # Robert Schumann
-    'spotify:artist:4NJhFmfw43RLBLjQvxDuRS',    # Wolfgang Amadeus Mozart
-    'spotify:artist:2wOqMjp9TyABvtHdOSOTUS',    # Ludwig van Beethoven
-    'spotify:artist:459INk8vcC0ebEef82WjIK',    # Erik Satie
-    'spotify:artist:5aIqB5nVVvmFsvSdExz408',    # Johann Sebastian Bach
-    'spotify:artist:7jzR5qj8vFnSu5JHaXgFEr',    # Jean Sibelius
-    'spotify:artist:656RXuyw7CE0dtjdPgjJV6',    # Joseph Haydn
-    'spotify:artist:7ie36YytMoKtPiL7tUvmoE',    # Igor Stravinsky
-    'spotify:artist:3MKCzCnpzw3TjUYs2v7vDA'     # Pyotr Ilyich Tchaikovsky
+    'spotify:artist:7y97mc3bZRFXzT2szRM4L4',        # Frédéric Chopin
+    'spotify:artist:3MKCzCnpzw3TjUYs2v7vDA',        # Pyotr Ilyich Tchaikovsky
+    'spotify:artist:1Uff91EOsvd99rtAupatMP',        # Claude Debussy
+    'spotify:artist:4NJhFmfw43RLBLjQvxDuRS',        # Wolfgang Amadeus Mozart
+    'spotify:artist:2wOqMjp9TyABvtHdOSOTUS',        # Ludwig van Beethoven
+    'spotify:artist:0Kekt6CKSo0m5mivKcoH51',        # Sergei Rachmaninoff
+    'spotify:artist:6s1pCNXcbdtQJlsnM1hRIA',        # Dmitri Shostakovich
+    'spotify:artist:459INk8vcC0ebEef82WjIK',        # Erik Satie
+    'spotify:artist:5aIqB5nVVvmFsvSdExz408',        # Johann Sebastian Bach
+    'spotify:artist:5ihY290YPGc3aY2xTyx7Gy',        # Edvard Grieg
+    'spotify:artist:6n7nd5iceYpXVwcx8VPpxF',        # Antonín Dvořák
+    'spotify:artist:2QOIawHpSlOwXDvSqQ9YJR',        # Antonio Vivaldi
+    'spotify:artist:5yxyJsFanEAuwSM5kOuZKc',        # London Symphony Orchestra
+    'spotify:artist:6HclTVD03WSY6GUpN16BkP',        # Claudio Abbado
+    'spotify:artist:436sYg6CZhNefQJogaXeK0',        # Camille Saint-Saëns
+    'spotify:artist:5wTAi7QkpP6kp8a54lmTOq',        # Johannes Brahms
+    'spotify:artist:0hIG9FXgjQxT8fKaYceFbA',        # Itzhak Perlman
+    'spotify:artist:6uRJnvQ3f8whVnmeoecv5Z',        # Berliner Philharmoniker
+    'spotify:artist:003f4bk13c6Q3gAUXv7dGJ',        # Wiener Philharmoniker
+    'spotify:artist:2p0UyoPfYfI76PCStuXfOP',        # Franz Schubert
+    'spotify:artist:77CaCn32H4mOMQA7UElzfF',        # Academy of St. Martin in the Fields
+    'spotify:artist:7jzR5qj8vFnSu5JHaXgFEr',        # Jean Sibelius
+    'spotify:artist:5zCaQxjl110XTrm4LQ1CxY',        # Herbert von Karajan
+    'spotify:artist:1QL7yTHrdahRMpvNtn6rI2',        # George Frideric Handel
+    'spotify:artist:78sEozQOEJxzXegUuqRSgH',        # Daniel Barenboim
+    'spotify:artist:1JOQXgYdQV2yfrhewqx96o',        # Giuseppe Verdi
+    'spotify:artist:2P6ygesd9xg5DPOBnda2jg',        # Arvo Pärt
+    'spotify:artist:69lxxQvsfAIoQbB20bEPFC',        # Philip Glass
+    'spotify:artist:1385hLNbrnbCJGokfH2ac2',        # Franz Liszt
+    'spotify:artist:17hR0sYHpx7VYTMRfFUOmY',        # Maurice Ravel
+    'spotify:artist:4kHtgiRnpmFIV5Tm4BIs8l',        # Sergei Prokofiev
+    'spotify:artist:2gClsBep1tt1rv1CN210SO',        # Gabriel Fauré
+    'spotify:artist:3gacryguGmpmCvgPGt2CBI',        # New York Philharmonic
+    'spotify:artist:7ie36YytMoKtPiL7tUvmoE',        # Igor Stravinsky
+    'spotify:artist:13dkPjqmbcchm8cXjEJQeP',        # Glenn Gould
+    'spotify:artist:430byzy0c5bPn5opiu0SRd',        # Edward Elgar
+    'spotify:artist:62TD7509VQIxUe4WpwO0s3',        # Johann Pachelbel
+    'spotify:artist:6nZiWg5ZB511S24WmB7TCV',        # Alexander Scriabin
+    'spotify:artist:6MF58APd3YV72Ln2eVg710',        # Felix Mendelssohn
+    'spotify:artist:2ANtgfhQkKpsW6EYSDqldz',        # Gustav Mahler
+    'spotify:artist:656RXuyw7CE0dtjdPgjJV6',        # Joseph Haydn
+    'spotify:artist:2D7RkvtKKb6E5UmbjQM1Jd',        # Georges Bizet
+    'spotify:artist:2LmyJyCF5V1eQyvHgJNbTn',        # Leonard Bernstein
+    'spotify:artist:3tMLo1k3iUo82coMLWXzxq',        # Henry Purcell
+    'spotify:artist:0OzxPXyowUEQ532c9AmHUR',        # Giacomo Puccini
+    'spotify:artist:5dmMpIyAVaH6b9FLFgWPrF',        # Arcangelo Corelli
+    'spotify:artist:5WIoytpqi3VWoFSHnl49in',        # Aram Khachaturian
+    'spotify:artist:20iZXzMb8LoWXOeca32i82',        # Vladimir Ashkenazy
+    'spotify:artist:4Ws5hSoABAwvGJ4LhHwHgq',        # Vladimir Horowitz
+    'spotify:artist:66MvLAvLznk5UOvASVGjk4',        # Martha Argerich
+    'spotify:artist:1C1x4MVkql8AiABuTw6DgE',        # Richard Wagner
+    'spotify:artist:5goS0v24Fc1ydjCKQRwtjM',        # Johann Strauss II
+    'spotify:artist:3meioy7GWDwpwmjv2LPyAb',        # Carl Philipp Emanuel Bach
+    'spotify:artist:1M9AXZkNPdOd1IPEsQsXnT',        # Léo Delibes
+    'spotify:artist:0roWUeP7Ac4yK4VN6L2gF4',        # Gioachino Rossini
+    'spotify:artist:2fg5h5pzleqI4RjFopWroj',        # Georg Philipp Telemann
+    'spotify:artist:4JSWO1Pf2zV991fq64uAce',        # Jean-Philippe Rameau
+    'spotify:artist:7vfydQ0nVBVgJ0ajs8EtRM',        # Christoph Willibald Gluck
+    'spotify:artist:284mnx33IWcymQEpMxyfHl',        # Modest Mussorgsky
+    'spotify:artist:5B7uXBeLc2TkR5Jk23qKIZ',        # Gustav Holst
+    'spotify:artist:50hiYMhi0g77BOkkQ4zebB',        # Mstislav Rostropovich
+    'spotify:artist:5zyNXVd952fWOjkdGHCvPd',        # Béla Bartók
+    'spotify:artist:17OArJzEhRR3OmhtGcnfBq',        # Tomaso Albinoni
+    'spotify:artist:25Eab1kIY1gh0Yo1oV04G4',        # Bedřich Smetana
+    'spotify:artist:34MYamymtmnsmpwbqydd7I',        # Alexander Borodin
+    'spotify:artist:7KkUirCiJZhgRN3NbgG98L',        # Ottorino Respighi
+    'spotify:artist:42Vmza0WYHdhsgxFmf9Tui',        # Charles Gounod
+    'spotify:artist:39FC9x5PaTNYHp5hwlaY4q',        # Niccolò Paganini
+    'spotify:artist:4XDJurjQCnWLlE7KLZCT9x',        # Samuel Barber
+    'spotify:artist:2prZJWfQMnIgwUKxKcBxH7',        # Pierre Boulez
+    'spotify:artist:7wNkISK49lKeXuRaZcQVFe',        # Ralph Vaughan Williams
+    'spotify:artist:4IAWVxo2fpTBPn6k7GZ3eY',        # Francis Poulenc
+    'spotify:artist:1p6wR69pnH9LBWZvwliuz2',        # Carl Maria von Weber
+    'spotify:artist:6pAwHPeExeUbMd5w7Iny6D',        # Richard Strauss
+    'spotify:artist:0bjdfjE8XbLa2Odstu6E1E',        # Maria Callas
+    'spotify:artist:1YuknfkSYTTbolRpwZBOv4',        # George Gershwin
+    'spotify:artist:5N1GUHhFMRFFgMTjSOJDb9',        # Arthur Rubinstein
+    'spotify:artist:2yzaWNFV3cxmcRZtwtr5WC',        # Clara Schumann
+    'spotify:artist:3eqv9LW7ZXlYWXVFjHS4Mr',        # Joaquín Rodrigo
+    'spotify:artist:2l4vGfFV7e46yO8lxfxR76',        # Luigi Boccherini
+    'spotify:artist:2kXJ68O899XvWOBdpzlXgs',        # Nikolai Rimsky-Korsakov
+    'spotify:artist:7MJ1pB5d6Vjmzep2zQlorn',        # Benjamin Britten
+    'spotify:artist:19v0Cnq5D4SKom8SVlXhkw',        # Jean-Baptiste Lully
+    'spotify:artist:0mFblCBw0GcoY7zY1P8tzE',        # Domenico Scarlatti
+    'spotify:artist:1AoIc5YFH0aSFc4mKqBEeB',        # Jules Massenet
+    'spotify:artist:65YhYi4Fz5Ibgq7ueev2Rm',        # Frederick Delius
+    'spotify:artist:1aVONoJ0EM97BB26etc1vo',        # Steve Reich
+    'spotify:artist:2jCGEMSZXMSOImpD8sqo56',        # Gaetano Donizetti
+    'spotify:artist:1mhwtKvBm1ncoOrUtnQUgk',        # John Dowland
+    'spotify:artist:78t7WfwhjZLkDHN5QKwS5u',        # Alexander Glazunov
+    'spotify:artist:5iAhVgz6P8Nylxijb0C65v',        # Claudio Monteverdi
+    'spotify:artist:4kVLtXuKJTE7SgYd2sQtaL',        # François Couperin
+    'spotify:artist:3Z5fRknMBBNfCw6pkgR9S8',        # Pietro Mascagni
+    'spotify:artist:08ewVs9B2qQZ2Gy3xfTFyo',        # Giovanni Battista Pergolesi
+    'spotify:artist:5U827e4jbYz6EjtN0fIDt9',        # Arnold Schoenberg
+    'spotify:artist:2xt1t3lfZ5FGaEKrs0jp0d',        # Hildegard von Bingen
+    'spotify:artist:0HdNDZaNm7xLt18v9aWDfe',        # Fritz Kreisler
+    'spotify:artist:5BcbyYRgvvhfVGmCJSjHlT',        # Manuel de Falla
+    'spotify:artist:46xXzoOdtD7SL2PTRq5irp',        # Leoš Janáček
+    'spotify:artist:2YfFYZnshSzgfLsKZMM4VL',        # Dietrich Buxtehude
+    'spotify:artist:1C3sffOOvQNUwg4YIsvKqy',        # César Franck
+    'spotify:artist:2bM3j1JQWBkmzuoZKu4zj2',        # Anton Bruckner
+    'spotify:artist:46x75iFAkipaG5IEAFN4L4',        # Giuseppe Tartini
+    'spotify:artist:6AphpMkKDU4I3Z6XaGu1UV',        # Vincenzo Bellini
+    'spotify:artist:01UzgUtDa2NqWuvUjrrl3p',        # Heitor Villa-Lobos
+    'spotify:artist:4hBgCK8FyJ5bV36ZZLB56X',        # Sviatoslav Richter
+    'spotify:artist:0BS8IrFmvK1tXenMULjwem',        # Giovanni Pierluigi da Palestrina
+    'spotify:artist:0nJvyjVTb8sAULPYyA1bqU',        # Aaron Copland
+    'spotify:artist:0qEO82Hj3SvjoNyEfKpRku',        # Krzysztof Penderecki
+    'spotify:artist:35OhI7DSls022v9Bz9r0VZ',        # John Adams
+    'spotify:artist:0521x50ZcNqqT1fKMJg5c5',        # Max Bruch
+    'spotify:artist:3iPWeMA0avSRKrPKcLaTDE',        # Marin Marais
+    'spotify:artist:11T8SOX82xraocZzUXzkvM',        # Hector Berlioz
+    'spotify:artist:5HGksnSVY91s4g1SOHHACw',        # Alessandro Scarlatti
+    'spotify:artist:4sbcjbyksdT4dJ5Lh0SvZp',        # Isaac Albéniz
+    'spotify:artist:3u1fWmwpwPOmMelTAo0Gb8',        # Paul Hindemith
+    'spotify:artist:4WMHJgeJCssiGW3ssUT7eb',        # Johann Strauss I
+    'spotify:artist:1zb5zmIuX2lTbzcn7YeQlg',        # György Ligeti
+    'spotify:artist:1Z3fF5lZdCM0ZHugkGoH8s',        # John Cage
+    'spotify:artist:3L1iFJfRzE7yzilr2yGl6t',        # Pablo de Sarasate
+    'spotify:artist:2j7yGPnVrdIHjHwtyLPJ2z',        # William Byrd
+    'spotify:artist:5OvQ8g54fWhffM2x8eCc6s',        # Marc-Antoine Charpentier
+    'spotify:artist:088fpww3Ae4U9cMZv5O6m8',        # Amilcare Ponchielli
+    'spotify:artist:6CS9O2pE67oq44GZuBEBuD',        # Olivier Messiaen
+    'spotify:artist:2kMM5v3f7rti00gIbVc1Tk',        # Heinrich Ignaz Franz von Biber
+    'spotify:artist:5p6TCmGe9IygOnvamIaZpA',        # Toru Takemitsu
+    'spotify:artist:6cg7ooZDeSSLYVTWGpASjX',        # Anton Webern
+    'spotify:artist:4Fw8f0YbjBs9suGEwInPJe',        # Heinrich Schütz
+    'spotify:artist:3KpcdlqCaWWruPfmM2rWy1',        # Paul Dukas
+    'spotify:artist:7wEcQTGkugQxXzikls4izh',        # Henryk Wieniawski
+    'spotify:artist:0OTPgUKkDp058dC3czXFM2'         # Michael Praetorius
 ]
